148a5f13384ec9345ba980c032eb3a3e7de5d94d
vethajerome <dvethajerome@gmail.com>
2025-06-08 23:25:46 +0530

    Updated entire project with latest changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,92 +1,13 @@
-// import React from "react";
-// import { Box, Typography, Divider, Container } from "@mui/material";
-// import { styled } from "@mui/material/styles";
-
-// const ThemedSection = styled(Box)(({ theme }) => ({
-//   marginBottom: theme.spacing(4),
-//   padding: theme.spacing(4),
-//   backgroundColor:
-//     theme.palette.mode === "dark"
-//       ? theme.palette.grey[900]
-//       : theme.palette.grey[100],
-//   color:
-//     theme.palette.mode === "dark"
-//       ? theme.palette.common.white
-//       : theme.palette.text.primary,
-//   borderRadius: theme.shape.borderRadius,
-//   boxShadow:
-//     theme.palette.mode === "dark"
-//       ? `0 4px 8px ${theme.palette.grey[800]}`
-//       : `0 4px 8px ${theme.palette.grey[300]}`,
-// }));
-
-// const About = ({ theme }) => {
-//   return (
-//     <div>
-//       <br></br>
-//       <br></br>
-//       <Container maxWidth="lg">
-//         <ThemedSection>
-//           <Typography variant="h4" gutterBottom fontWeight={"bold"}>
-//             Our Mission
-//           </Typography>
-//           <Typography variant="body1">
-//             Our mission is to empower farmers and gardeners with an easy-to-use
-//             tool for diagnosing plant diseases. By leveraging advanced AI and
-//             machine learning technologies, we aim to improve crop yields and
-//             promote sustainable agricultural practices.
-//           </Typography>
-//         </ThemedSection>
-//         <Divider />
-//         <ThemedSection>
-//           <Typography variant="h4" gutterBottom fontWeight={"bold"}>
-//             How to Use
-//           </Typography>
-//           <Typography variant="body1">
-//             <ol>
-//               <li>Click the Detector section on Top of Navbar</li>
-//               <li>Take a clear photo of the affected plant area.</li>
-//               <li>
-//                 Upload the photo to our application by clicking the "Choose
-//                 File" button.
-//               </li>
-//               <li>Wait a few moments for our AI to analyze the image.</li>
-//               <li>Receive a diagnosis and suggested treatment options.</li>
-//             </ol>
-//           </Typography>
-//         </ThemedSection>
-//         <Divider />
-//         <ThemedSection>
-//           <Typography variant="h4" gutterBottom fontWeight={"bold"}>
-//             Technology Used
-//           </Typography>
-//           <Typography variant="body1">
-//             Our application uses advanced machine learning algorithms,
-//             particularly convolutional neural networks (CNNs), Deep learning, to
-//             accurately diagnose plant diseases. The model is trained on a
-//             comprehensive dataset, enabling it to identify a wide variety of
-//             diseases with high accuracy and speed.
-//           </Typography>
-//         </ThemedSection>
-//       </Container>
-//     </div>
-//   );
-// };
-
-// export default About;
-
 import React, { useState, useRef, useEffect } from "react";
 import {
   Box,
   Typography,
   Divider,
   Container,
-  IconButton,
   Menu,
   MenuItem,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import MessageIcon from "@mui/icons-material/Message"; // Import the message icon
 import ChatbotComponent from "./Navbar/ChatbotComponent";
 
 const ThemedSection = styled(Box)(({ theme }) => ({
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
-import { TextField, Button, Typography, Box, Card, CardContent, CardActions } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import {
+  TextField,
+  Button,
+  Typography,
+  Box,
+  Card,
+  CardContent,
+  CardActions,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { Link, useNavigate } from "react-router-dom";
+
 const StyledCard = styled(Card)(({ theme }) => ({
   minWidth: 275,
   padding: theme.spacing(2),
-  backgroundColor: 'rgba(255, 255, 255, 0.8)',
-  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-  transition: 'transform 0.3s, box-shadow 0.3s',
-  '&:hover': {
-    transform: 'scale(1.05)',
-    boxShadow: '0 8px 16px rgba(0, 0, 0, 0.3)',
-  }
+  backgroundColor: "rgba(255, 255, 255, 0.8)",
+  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+  transition: "transform 0.3s, box-shadow 0.3s",
+  "&:hover": {
+    transform: "scale(1.05)",
+    boxShadow: "0 8px 16px rgba(0, 0, 0, 0.3)",
+  },
 }));
 
 const StyledButton = styled(Button)(({ theme }) => ({
@@ -20,75 +28,81 @@ const StyledButton = styled(Button)(({ theme }) => ({
   marginBottom: theme.spacing(2),
   backgroundColor: theme.palette.primary.main,
   color: theme.palette.common.white,
-  '&:hover': {
+  "&:hover": {
     backgroundColor: theme.palette.primary.dark,
-  }
+  },
 }));
 
 const Register = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [error, setError] = useState('');
-  const navigate=useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
   const handleRegister = (e) => {
     e.preventDefault();
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const passwordRegex = /^.{6,}$/;
     const phoneRegex = /^[0-9]{10}$/;
     if (!name || !email || !phone || !password || !confirmPassword) {
-      setError('All fields are required');
+      setError("All fields are required");
       return;
     }
 
     if (!emailRegex.test(email)) {
-      setError('Invalid email address');
+      setError("Invalid email address");
       return;
     }
 
     if (!phoneRegex.test(phone)) {
-      setError('Invalid phone number. It should be a 10-digit number.');
+      setError("Invalid phone number. It should be a 10-digit number.");
       return;
     }
 
     if (!passwordRegex.test(password)) {
-      setError('Password must be at least 6 characters long');
+      setError("Password must be at least 6 characters long");
       return;
     }
 
     if (password !== confirmPassword) {
-      setError('Passwords do not match');
+      setError("Passwords do not match");
       return;
     }
 
-    setError('');
-    const user={name,email,phone,password};
+    setError("");
+    const user = { name, email, phone, password };
     console.log(user);
-    fetch("http://localhost:8080/users/add",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(user)}).then(()=>{
-      console.log("New user added to Database")
-    })
+    fetch("http://localhost:8080/users/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    }).then(() => {
+      console.log("New user added to Database");
+    });
     navigate("/");
   };
 
   return (
     <Box
       sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        minHeight: '100vh',
-        backgroundImage: 'url("https://img.freepik.com/free-photo/tropical-palm-leaves-pattern-background-green-monstera-tree-foliage-decoration-design-plant-with-exotic-leaf-closeup_90220-1135.jpg")',
-        
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        backgroundImage:
+          'url("https://img.freepik.com/free-photo/tropical-palm-leaves-pattern-background-green-monstera-tree-foliage-decoration-design-plant-with-exotic-leaf-closeup_90220-1135.jpg")',
+
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        width: "100%",
+        height: "100%",
+        position: "absolute",
         top: 0,
-        left: 0
+        left: 0,
       }}
     >
       <StyledCard>
@@ -104,7 +118,11 @@ const Register = () => {
           <Box
             component="form"
             onSubmit={handleRegister}
-            sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
             <TextField
               margin="normal"
@@ -165,13 +183,9 @@ const Register = () => {
               onChange={(e) => setConfirmPassword(e.target.value)}
             />
             <CardActions>
-              <StyledButton
-                type="submit"
-                fullWidth
-              >
+              <StyledButton type="submit" fullWidth>
                 Register
               </StyledButton>
-              
             </CardActions>
           </Box>
         </CardContent>
